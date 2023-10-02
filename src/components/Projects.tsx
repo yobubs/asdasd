77253f2402b18c6358f5fa6b0676035536bff51e
minards <minar@gmail.com>
2023-10-02 14:00:26 +0800

    sad

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,7 @@
 const Projects = () => {
   return (
     <div className="h-screen w-full  bg-gradient-to-b from-violet-500 to-violet-900 py-7">
-      <h1 className="text-center">Projects</h1>
+      <h1 className="text-center">Projects </h1>
     </div>
   )
 }
